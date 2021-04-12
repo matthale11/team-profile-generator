5beeebd5d377e457e60e4585cf98d8e03522e1c7
Matt Hale <mhale@factharbor.com>
2021-04-12 15:01:20 -0400

    pseudo coding

---
+++ src/template.js
@@ -0,0 +1,30 @@
+// Create div page elements with bootstrap
+
+// Use template literals to pass in object values...
+
+// In the src folder, create a page-template.js
+// const generateTeam = team => {
+// const  generateManager = manager = {
+//     ` return ${manager.getName()} etc etc`
+// }
+// const html = [];
+//     html.push(team
+//         .filter(employee => employee.getRole() === "Manager")
+//         .map(manager => generateManager(manager))
+//     );
+//     return html.join("");
+// }
+// module.exports = team => {
+//   HTML TEMPLATE
+//       WHERE YOU WANT TO SHOW THE USER CONTENT
+//     <div>
+//       ${generateTeam(team)}
+//     </div>
+// }
+
+// Export an anonymous function (see class example) rather than an object
+module.exports = team => {
+    return `
+    PASTE HTML 
+    `
+}
