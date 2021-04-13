fc5b155e315096fb5f1a0307c3f2dbcbf521b097
Matt Hale <mhale@factharbor.com>
2021-04-13 07:44:58 -0400

    created employee objects

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // This is where your main logic will lie
+// Require page template which provides an anonymous function that can accept a parameter
 const path = require('path');
 const fs = require('fs');
-
-// Require page template which provides an anonymous function that can accept a parameter
 const pageTemplate = require('./src/template');
 
 pageTemplate(answers_from_inquirer_prompts);
@@ -13,21 +12,24 @@ pageTemplate(answers_from_inquirer_prompts);
 
 // If this is your entry point, you must do your inquirer here
 
-// This is where do you your FS write file stuff
+// Save new HTML files to the dist directory
+const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-// In index.js: 
-// const path = require("path");
-// const fs = require("fs");
-// const OUTPUT_DIR = path.resolve(__dirname, "dist")
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// function runApp() {
-//   ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-//   function buildTeam() {
-//     // Create the output directory if the output path doesn't exist
-//     if (!fs.existsSync(OUTPUT_DIR)) {
-//       fs.mkdirSync(OUTPUT_DIR)
-//     }
-//     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
-//   }
-// }
-// runApp();
+function runApp() {
+  ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
+
+    let array = [];
+    .then((response) => {
+    const employee = new Employee(response.name, response.id);
+    array.push(employee);
+    }
+  function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
+}
+runApp();
--- src/template.js
+++ src/template.js
@@ -1,30 +1,21 @@
 // Create div page elements with bootstrap
 
-// Use template literals to pass in object values...
-
-// In the src folder, create a page-template.js
-// const generateTeam = team => {
-// const  generateManager = manager = {
-//     ` return ${manager.getName()} etc etc`
-// }
-// const html = [];
-//     html.push(team
-//         .filter(employee => employee.getRole() === "Manager")
-//         .map(manager => generateManager(manager))
-//     );
-//     return html.join("");
-// }
-// module.exports = team => {
-//   HTML TEMPLATE
-//       WHERE YOU WANT TO SHOW THE USER CONTENT
-//     <div>
-//       ${generateTeam(team)}
-//     </div>
-// }
+const createTeam = team => {
+const  createManager = manager = {
+    ` return ${manager.getName()} etc etc`
+}
+const html = [];
+    html.push(team
+        .filter(employee => employee.getRole() === "Manager")
+        .map(manager => createManager(manager))
+    );
+    return html.join("");
+}
 
 // Export an anonymous function (see class example) rather than an object
+// Use template literals to pass in object values
 module.exports = team => {
     return `
-    PASTE HTML 
+    PASTE HTML ${createTeam(team)}
     `
 }
