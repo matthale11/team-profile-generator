b7f13180955ae643ed053a7c1611ceb5bb409d12
Matt Hale <mhale@factharbor.com>
2021-04-13 10:03:39 -0400

    modified inquirer prompts and classes

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
-// Require page template which provides an anonymous function that can accept a parameter
-const path = require('path');
-const fs = require('fs');
-const pageTemplate = require('./src/template');
+const path = require("path");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const Manager = require("./lib/manager");
+const render = require("./src/template");
 
-pageTemplate(answers_from_inquirer_prompts);
+// pageTemplate(answers_from_inquirer_prompts);
 
 // Save new HTML files to the dist directory
-const OUTPUT_DIR = path.resolve(__dirname, "dist")
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Create an empty array to hold team members outside of inquirer
+const teamArray = [];
+
 function runApp() {
-    let array = [];
+  function addManager() {
     inquirer
-    .prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: "What is the manager's name?",
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: "What is the manager's employee ID?",
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: "What is the manager's email address?",
-    },
-    {
-        type: 'input',
-        name: 'office',
-        message: "What is the manager's office number?",
-    },
-    {
-        type: 'list',
-        name: 'role',
-        choices: ['Engineer', 'Intern'],
-        message: "Please select engineer or intern",
-    },
-    ])
-    .then((response) => {
-        const employee = new Employee(response.name, response.id);
-        array.push(employee);
-        }
-    
-        function buildTeam() {
-    // Create the output directory if the output path doesn't exist
+      .prompt([
+        {
+          type: "input",
+          name: "mgrName",
+          message: "What is the team manager's name?",
+        },
+        {
+          type: "input",
+          name: "mgrId",
+          message: "What is the manager's employee ID?",
+        },
+        {
+          type: "input",
+          name: "mgrEmail",
+          message: "What is the manager's email address?",
+        },
+        {
+          type: "input",
+          name: "office",
+          message: "What is the manager's office number?",
+        },
+        {
+          type: "list",
+          name: "role",
+          choices: ["Engineer", "Intern"],
+          message: "Please select engineer or intern",
+        },
+      ])
+      .then((response) => {
+        const manager = new Manager(
+          response.mgrName,
+          response.mgrId,
+          response.mgrEmail,
+          response.office
+        );
+        teamArray.push(manager);
+        // TODO: Execute after managers, engineers, and interns are added
+        buildTeam();
+      });
+  }
+  // function addEngineer() {
+
+  // }
+  // function addIntern() {
+
+  // }
+
+  function buildTeam() {
+    // Create an output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
-      fs.mkdirSync(OUTPUT_DIR)
+      fs.mkdirSync(OUTPUT_DIR);
     }
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+    fs.writeFileSync(outputPath, render(teamArray), "utf-8");
   }
 }
 
--- src/template.js
+++ src/template.js
@@ -1,21 +1,22 @@
 // Create div page elements with bootstrap
 
-const createTeam = team => {
-const  createManager = manager = {
-    ` return ${manager.getName()} etc etc`
-}
-const html = [];
-    html.push(team
-        .filter(employee => employee.getRole() === "Manager")
-        .map(manager => createManager(manager))
-    );
-    return html.join("");
-}
+// const createTeam = team => {
+// const  createManager = manager = {
+//     ` return ${manager.getName()} etc etc`
+// }
+// const html = [];
+//     html.push(team
+//         .filter(employee => employee.getRole() === "Manager")
+//         .map(manager => createManager(manager))
+//     );
+//     return html.join("");
+// }
 
 // Export an anonymous function (see class example) rather than an object
 // Use template literals to pass in object values
 module.exports = team => {
+    console.log(team)
     return `
-    PASTE HTML ${createTeam(team)}
+    PASTE HTML ${buildTeam(team)}
     `
 }
