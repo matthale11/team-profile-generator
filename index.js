8381010a8e87100e83667c909225ecce8bb3f458
Matt Hale <mhale@factharbor.com>
2021-04-13 09:21:12 -0400

    updated dummy formatting

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// This is where your main logic will lie
 // Require page template which provides an anonymous function that can accept a parameter
 const path = require('path');
 const fs = require('fs');
@@ -6,25 +5,47 @@ const pageTemplate = require('./src/template');
 
 pageTemplate(answers_from_inquirer_prompts);
 
-// NOTE: Employee is the main class bc all engineers, interns, and managers are employees (extend from employee)
-
-// Index files are considered the entry point to your application
-
-// If this is your entry point, you must do your inquirer here
-
 // Save new HTML files to the dist directory
 const OUTPUT_DIR = path.resolve(__dirname, "dist")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 function runApp() {
-  ...Inquirer prompt and the functions that will ask users about manager, intern, and engineer.
-
     let array = [];
+    inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: "What is the manager's name?",
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "What is the manager's employee ID?",
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email address?",
+    },
+    {
+        type: 'input',
+        name: 'office',
+        message: "What is the manager's office number?",
+    },
+    {
+        type: 'list',
+        name: 'role',
+        choices: ['Engineer', 'Intern'],
+        message: "Please select engineer or intern",
+    },
+    ])
     .then((response) => {
-    const employee = new Employee(response.name, response.id);
-    array.push(employee);
-    }
-  function buildTeam() {
+        const employee = new Employee(response.name, response.id);
+        array.push(employee);
+        }
+    
+        function buildTeam() {
     // Create the output directory if the output path doesn't exist
     if (!fs.existsSync(OUTPUT_DIR)) {
       fs.mkdirSync(OUTPUT_DIR)
@@ -32,4 +53,5 @@ function runApp() {
     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
   }
 }
+
 runApp();
